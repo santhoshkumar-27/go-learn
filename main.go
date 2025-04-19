14d2719803bfb1fd6d414935b54b7fed59d4a48e
santhoshkumar <its.santhoshkumar@outlook.com>
2025-04-19 19:43:27 +0530

    feat: get input from users

--- main.go
+++ main.go
@@ -8,7 +8,13 @@ var conferenceName1 string = "Go Conference"
 
 const conferenceTicket = 50
 
+var userName string
+var userAge uint
+
 func main() {
+	dummmssssy := "value" // only apply for var
+
+	fmt.Printf("Conference is %T \n %v", userName, dummmssssy)
 	fmt.Println("Welcome to our", conferenceName, "booking application")
 	fmt.Println("Get you ticket from here to watch IPl")
 	fmt.Println(conferenceName, conferenceTicket)
@@ -16,4 +22,17 @@ func main() {
 	fmt.Printf("replace value %v ", conferenceName1)
 
 	fmt.Printf("adsfdsaf %v", 1+2)
+
+	// fmt.Scan(userName)  // we are just passing values only here so they can't stop
+	fmt.Scan(&userName) // we are passing as pointer of (memory address) but here they wait
+
+	// default value will be empty string "",
+	// userName = "Santhosh"
+	// default value will be 0,
+	userAge = 26
+
+	fmt.Println(userAge)
+	// fmt.Println(&userAge) // Pointer it will return the memory address
+
+	fmt.Print(userName, userAge)
 }
