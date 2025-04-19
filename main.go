3e620b099c2a4ea1d7c89d8fc732ebce85346981
santhoshkumar <its.santhoshkumar@outlook.com>
2025-04-19 16:11:06 +0530

    feat: variable declarations

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+var conferenceName = "Go Conference"
+
+var conferenceName1 string = "Go Conference"
+
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("Welcome to our conference booking application")
+	fmt.Println("Get you ticket from here to watch IPl")
+	fmt.Println(conferenceName)
+
+	fmt.Printf("replace value %v", conferenceName1)
 }
