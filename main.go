2d930676b43a490528aa35186707a80577cbacc0
santhoshkumar <its.santhoshkumar@outlook.com>
2025-04-21 07:43:01 +0530

    feat: array & slice are done

--- main.go
+++ main.go
@@ -15,6 +15,10 @@ func main() {
 	var bookings = [50]string{"Santhoshkumar viswanathan"}
 	// var bolling [50]string
 
+	// dynamic array length
+	// var bookings1 = []string{}
+	var bookings1 []string
+
 	var fullName string
 	var age uint
 
@@ -25,11 +29,21 @@ func main() {
 
 	bookings[0] = fullName
 
+	bookings1 = append(bookings1, fullName)
+
 	fmt.Printf("Hi %v this your name, and this is your age %v \n", fullName, age)
 
+	// fixed length
 	fmt.Printf("The whole array %v \n", bookings)
 	fmt.Printf("The first array %v \n", bookings[0])
 	fmt.Printf("The type of  array %T \n", bookings)
 	fmt.Printf("The size of  array %v \n", len(bookings))
+
+	// dynamic length
+
+	fmt.Printf("The whole array %v \n", bookings1)
+	fmt.Printf("The first array %v \n", bookings1[0])
+	fmt.Printf("The type of  array %T \n", bookings1)
+	fmt.Printf("The size of  array %v \n", len(bookings1))
 
 }
