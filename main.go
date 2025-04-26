33767fb7458bcbd03d27d0942bd35ab6b7907589
santhoshkumar <its.santhoshkumar@outlook.com>
2025-04-26 17:09:40 +0530

    feat: initial condition

--- main.go
+++ main.go
@@ -1,12 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
+	var bookings = [3]string{"santhosh kumar", "kum ar", "santh osh"}
+
 	// infinite loop
+	// for {
+	// 	fmt.Println("for loop for ")
+
+	// }
+
+	var incrementer uint
+
+	// normal loop with break
 	for {
 		fmt.Println("for loop for ")
+
+		incrementer += 1
+
+		if incrementer == 5 {
+			break // break will terminate the loop
+			// continue // will skip the current iteration and skip to next iteratio
+		}
 	}
 
+	firstNames := []string{}
+
+	// for-each
+	// basically it has for index, booking := range bookings
+	// if i use underscore it represents as blank operator
+	for _, booking := range bookings {
+		var names = strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
+	}
+
+	var i = 0
+
+	for i < 5 {
+		fmt.Println("condtions", i)
+
+		i++
+	}
+
+	fmt.Println("Dummy data", firstNames, len(firstNames))
+
 }
+
+// if condition {
+// } else if condition {
+// } else {
+// }
